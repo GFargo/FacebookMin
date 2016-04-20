faec2bdf5c81c86f5a9ea96ad048bfcba7b771ec
Griffen Fargo <griffen.fargo@lycos-inc.com>
2016-04-20 16:59:59 -0400

    Updates FacebookMin with updated styling / adds steve / updates readme

--- Readme.md
+++ Readme.md
@@ -1,11 +1,21 @@
-# FacebookMin - Remove the Noise
+# FacebookMin - Minimize the Noise
 
-This is brutally simple Chrome Extension that removes the "News Feed" whenever visiting Facebook.
+One day I woke up and was tired of the [time sucking](http://www.urbandictionary.com/define.php?term=time+suck) characteristics about Facebook.  Instead of simply deleting my account and in turn losing access to my friends I decided to get a little creative.  Enter FacebookMin...
 
-![Homepage](https://i.imgur.com/RpJxWPO.png)
-![Individual Post View](https://i.imgur.com/E3tUzVu.png)
+This Chrome Extension will automatically remove the "News Feed" when visiting Facebook.
+
+User Notifications, Chat, Events and other _social networking_ features have been left intact.  
 
 
-#### Bugs
+## Screenshots
 
-Currently it will hide the content of user pages entirely.  Oh well!
+![Homepage](https://i.imgur.com/GbthfaC.png)
+
+
+#### ToDo / Bugs
+
+- [ ] Pack up extension & Release on [Chrome Store](https://chrome.google.com/webstore/category/extensions)
+- [ ] "New Posts" notification still appears in center of screen where feed layout is when a new post appears.
+
+
+Please submit any bugs or issues through Github, Thanks!
--- src/bg/background.js
+++ src/bg/background.js
@@ -1,11 +1,36 @@
-chrome.history.onVisited.addListener(urlCheck);
+// chrome.history.onVisited.addListener(urlCheck);
 
 
-function urlCheck (result) {
-    console.log('url: ', result);
+// function urlCheck (result) {
+//     console.log('url: ', result);
 
-    if (result.url.indexOf("facebook") !== -1) {
-        console.log('Facebook Detected!');
-        // $('#stream_pagelet').remove();
+//     if (result.url.indexOf("facebook") !== -1) {
+//         console.log('Facebook Detected!');
+//         // $('#stream_pagelet').remove();
+
+//         document.getElementById('globalContainer').style.BackgroundImage = chrome.extension.getURL("src/img/steve.gif");
+
+//     }
+// }
+console.log('Background running');
+
+
+chrome.webNavigation.onCompleted.addListener(function(details) {
+    console.log('Comleted load', details);
+    if (details.url.indexOf('facebook') !== false) {
+        console.log('Facebook Found in URL');
+
+        imgURL = chrome.extension.getURL("src/img/steve.gif");
+
+        console.log('Steve URL: ', imgURL);
+
+        chrome.tabs.executeScript(details.tabId, {
+            code: 'document.getElementsByClassName("home_right_column")[0].style.backgroundImage =  "url(' + imgURL +')";'
+        });
     }
-}
+}, {
+    url: [{
+        // Runs on example.com, example.net, but also example.foo.com
+        hostContains: '.facebook.'
+    }],
+});
